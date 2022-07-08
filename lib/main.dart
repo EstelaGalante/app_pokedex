009a857032ab691666ee8759e2360a8e7b9a54a3
Estela <estelamfonseca@gmail.com>
2022-07-08 17:31:22 -0300

    App_Pokedex_VersaoFinal

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:app_pokedex/pokemon.dart';
+import 'package:flutter/material.dart';
+import 'detalhes.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Pokedex',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const HomePage());
+  }
+}
